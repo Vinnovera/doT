49593e22e6e5f8ef3f34c86c0404f04c9afb4b2d
Per Stenström <per.stenstrom@gmail.com>
2014-03-31 22:00:10 +0200

    Update tests to reflect intended changes

--- test/testdoT.js
+++ test/testdoT.js
@@ -16,7 +16,7 @@ describe('doT', function(){
 		it('should render the template', function(){
 		   assert.equal("<div>http</div>", basiccompiled({foo:"http"}));
 		   assert.equal("<div>http:&#47;&#47;abc.com</div>", basiccompiled({foo:"http://abc.com"}));
-		   assert.equal("<div></div>", basiccompiled({}));
+		   assert.equal("<div>{{!it.foo}}</div>", basiccompiled({}));
 		});
 	});
 
@@ -24,7 +24,27 @@ describe('doT', function(){
 		it('should render define', function(){
 		   assert.equal("<div>http</div>", definescompiled({foo:"http"}));
 		   assert.equal("<div>http:&#47;&#47;abc.com</div>", definescompiled({foo:"http://abc.com"}));
-		   assert.equal("<div></div>", definescompiled({}));
+		   assert.equal("<div>{{!it.foo}}</div>", definescompiled({}));
+		});
+	});
+});
+
+describe('doT Extended', function(){
+	var
+		namespacetemplate = "<div>{{!it.foo}}{{=part.foo}}</bar>",
+		namespacecompiled = doT.template(namespacetemplate);
+
+	describe('#template()', function(){
+		it('should return a function', function(){
+		   assert.equal("function", typeof namespacecompiled);
+		});
+	});
+
+	describe('namespace', function(){
+		it('should render it namespace', function(){
+		   assert.equal("<div>http{{=part.foo}}</div>", namespacecompiled({foo:"http"}));
+		   assert.equal("<div>http:&#47;&#47;abc.com{{=part.foo}}</div>", namespacecompiled({foo:"http://abc.com"}));
+		   assert.equal("<div>{{!it.foo}}{{=part.foo}}</bar>", namespacecompiled({}));
 		});
 	});
 });
